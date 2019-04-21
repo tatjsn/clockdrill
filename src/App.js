6dc37c978a58925b46aee9d148ec19908477807d
Taz Jackson <tatjsn@users.noreply.github.com>
2019-04-21 16:30:13 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,128 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { css, cx } from 'emotion';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import Clock from './Clock';
+
+const hours = [
+  'One',
+  'Two',
+  'Three',
+  'Four',
+  'Five',
+  'Six',
+  'Seven',
+  'Eight',
+  'Nine',
+  'Ten',
+  'Eleven',
+  'Twelve',
+];
+
+const mins = [
+  'Five',
+  'Ten',
+  'Quater',
+  'Twenty',
+  'Twenty five',
+  'Half',
+];
+
+const relatives = [
+  'Past',
+  'To',
+];
+
+const fullWidthControl = css`
+display: block;
+width: 100%;
+height: 40px;
+background-color: rgba(0, 0, 0, 0.1)
+`;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
+
+function App() {
+  const [score, setScore] = useState(0);
+  const [questionHour, setQuestionHour] = useState(getRandomInt(12));
+  const [questionMinute, setQuestionMinute] = useState(getRandomInt(11));
+  const [answerHour, setAnswerHour] = useState(0);
+  const [answerMinute, setAnswerMinute] = useState(0);
+  const [answerRelative, setAnswerRelative] = useState(0);
+
+  function handleSelectMinute(event) {
+    setAnswerMinute(mins.findIndex(item => item === event.target.value));
+  }
+
+  function handleSelectHour(event) {
+    setAnswerHour(hours.findIndex(item => item === event.target.value));
+  }
+
+  function handleSelectRelative(event) {
+    setAnswerRelative(relatives.findIndex(item => item === event.target.value));
+  }
+
+  function handleAnswer() {
+    let correct;
+
+    if (answerRelative === 0) {
+      // Past
+      correct = (answerHour === questionHour) && (answerMinute === questionMinute);
+    } else {
+      // To
+      correct = (answerHour === questionHour + 1) && (10 - answerMinute === questionMinute);
+    }
+
+    if (correct) {
+      setScore(score + 1);
+      setQuestionHour(getRandomInt(12));
+      setQuestionMinute(getRandomInt(11));
+    }
+  }
+
+  return (
+    <div className={css`
+position: relative;
+height: 100vh;
+padding-top: 40px;
+`}>
+      <Clock className={css`
+margin: 0 auto;
+`} hours={questionHour + 1} mins={(questionMinute + 1) * 5} />
+      <div className={css`
+position: absolute;
+top: 0;
+left: 0;
+width: 100%;
+line-height: 40px;
+`}>Score: {score}</div>
+      <div className={css`
+position: absolute;
+bottom: 0;
+left: 0;
+width: 100%;
+`}>
+        <select className={fullWidthControl} value={mins[answerMinute]} onChange={handleSelectMinute}>
+          {mins.map(item => (
+            <option key={item} value={item}>{item}</option>
+          ))}
+        </select>
+        <select className={fullWidthControl} value={relatives[answerRelative]} onChange={handleSelectRelative}>
+          {relatives.map(item => (
+            <option key={item} value={item}>{item}</option>
+          ))}
+        </select>
+        <select className={fullWidthControl} value={hours[answerHour]} onChange={handleSelectHour}>
+          {hours.map(item => (
+            <option key={item} value={item}>{item}</option>
+          ))}
+        </select>
+        <button className={fullWidthControl} type="button" onClick={handleAnswer}>Ok</button>
+      </div>
+    </div>
+  );
+}
+
 
 export default App;
---
+++ src/Clock.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { css, cx } from 'emotion';
+
+function clockToRadian(num) {
+  return (num * 2 - 6) * Math.PI / 12;
+}
+
+function Clock({ hours, mins, className }) {
+  return (
+    <div className={cx(css`
+position: relative;
+width: 300px;
+height: 300px;
+border: solid black 4px;
+border-radius: 50%;
+`, className)}>
+      {Array.from({ length: 12 }, (_, i) => i + 1).map(num => (
+        <div key={num} className={cx(css`
+position: absolute;
+width: 3rem
+height: 3rem;
+top: 50%;
+left: 50%;
+font-size: 24px;
+font-weight: bold;
+`, css`
+transform: translate(
+calc(${Math.cos(clockToRadian(num)) * 135}px - 50%),
+calc(${Math.sin(clockToRadian(num)) * 135}px - 50%)
+)
+`)}>{num}</div>
+      ))}
+      <div className={cx(css`
+position: absolute;
+width: 80px;
+height: 12px;
+background-color: black;
+top: 50%;
+left: 50%;
+transform-origin: center left;
+`, css`
+transform: translate(0, -50%) rotate(${clockToRadian(hours + mins / 60)}rad)
+`)}/>
+      <div className={cx(css`
+position: absolute;
+width: 120px;
+height: 10px;
+background-color: black;
+top: 50%;
+left: 50%;
+transform-origin: center left;
+`, css`
+transform: translate(0, -50%) rotate(${clockToRadian(mins / 5)}rad)
+`)}/>
+    </div>
+  );
+}
+
+export default Clock;
