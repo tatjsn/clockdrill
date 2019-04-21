0347eb453be1dd325c168aa2e66381ec17e5cf65
Taz Jackson <tatjsn@users.noreply.github.com>
2019-04-21 16:35:16 +0100

    Updates

---
+++ precache-manifest.598245f10d1f10f424aa4020f828b2fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42428fac58c4b9658235",
+    "url": "/clockdrill/static/js/runtime~main.8971ec5d.js"
+  },
+  {
+    "revision": "cad1e6fc375d284e97cb",
+    "url": "/clockdrill/static/js/main.23a9b1ad.chunk.js"
+  },
+  {
+    "revision": "9c4eac00c712b6a3cbca",
+    "url": "/clockdrill/static/js/2.e494d1d2.chunk.js"
+  },
+  {
+    "revision": "cad1e6fc375d284e97cb",
+    "url": "/clockdrill/static/css/main.bf2b714c.chunk.css"
+  },
+  {
+    "revision": "e0885d675d56e59c920b27f6b04e0fdc",
+    "url": "/clockdrill/index.html"
+  }
+];
